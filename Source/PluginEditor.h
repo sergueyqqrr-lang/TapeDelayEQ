e598597a87f5dc05e66c8923cc887d4a39b74720
sergueyqqrr-lang <sergueyqqrr@gmail.com>
2026-09-14 14:56:45 -0400

    Integrate HybridEQ with new EQ controls
    Added EQ controls and attachments for HybridEQ.

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "PluginProcessor.h"
+#include "DSP/HybridEQ.h"
 
 class TapeDelayEQAudioProcessorEditor : public juce::AudioProcessorEditor
 {
@@ -17,6 +18,12 @@ private:
 
     using SliderAttachment = juce::AudioProcessorValueTreeState::SliderAttachment;
     std::unique_ptr<SliderAttachment> delayTimeAtt, feedbackAtt, mixAtt, wowAtt, flutterAtt, satAtt;
+
+    // 12 bandas del EQ + saturación del EQ
+    static constexpr int numEqControls = HybridEQ::numBands + 1;
+    std::array<juce::Slider, numEqControls> eqSliders;
+    std::array<juce::Label,  numEqControls> eqLabels;
+    std::array<std::unique_ptr<SliderAttachment>, numEqControls> eqAtt;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (TapeDelayEQAudioProcessorEditor)
 };
